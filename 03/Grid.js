507103502e86e2a86cd90ccc703f099a7c9d908e
kevinkace <kevinacameron@hotmail.com>
2019-12-03 12:18:06 -0800

    Grid

--- 03/Grid.js
+++ 03/Grid.js
@@ -3,30 +3,74 @@ module.exports = class Grid {
         this.grid   = [];
         this.offset = offset;
 
-        this.x = offset + 1;
-        this.y = offset + 1;
+        this.x = offset;
+        this.y = offset;
 
-        for (let i = 0; i < offset; i++) {
+        for (let i = 0; i < 2 * offset; i++) {
             this.grid.push(new Array(2 * offset).fill("."));
         }
+
+        this.grid[this.y][this.x] = 0;
     }
 
     addLine(dir, len) {
         switch(dir) {
             case "U" :
-            case "R" :
-                const currRow = this.getCurrRow();
+                for (let i = 1; i <= len; i++) {
+                    this.y--;
 
-                for (let i = 0; i < len; i++) {
-                    currRow[this.x + i] = "-";
+                    if (this.currVal === 0) {
+                        return;
+                    }
+
+                    this.grid[this.y][this.x] = this.currVal === "-" ? "+" : "|";
                 }
+
+                break;
+            case "R" :
+                for (let i = 1; i <= len; i++) {
+                    this.x++;
+
+                    if (this.currVal === 0) {
+                        return;
+                    }
+
+                    this.currRow[this.x] = this.currVal === "|" ? "+" : "-";
+                }
+
                 break;
             case "D" :
+                for (let i = 1; i <= len; i++) {
+                    this.y++;
+
+                    if (this.currVal === 0) {
+                        return;
+                    }
+
+                    this.grid[this.y][this.x] = this.currVal === "-" ? "+" : "|";
+                }
+
+                break;
             case "L" :
+                for (let i = 1; i <= len; i++) {
+                    this.x--;
+
+                    if (this.currVal === 0) {
+                        return;
+                    }
+
+                    this.currRow[this.x] = this.currVal === "|" ? "+" : "-";
+                }
+
+                break;
         }
     }
 
-    getCurrRow() {
+    get currVal() {
+        return this.grid[this.y][this.x];
+    }
+
+    get currRow() {
         return this.grid[this.y];
     }
 
--- 03/grid.test.js
+++ 03/grid.test.js
@@ -1,7 +1,11 @@
 const Grid = require("./Grid");
 
-const grid = new Grid(5);
+const grid = new Grid(10);
 
 grid.addLine("R", 3);
+grid.addLine("U", 3);
+grid.addLine("L", 3);
+grid.addLine("D", 1);
+grid.addLine("R", 4);
 
 console.log(`${grid}`);
